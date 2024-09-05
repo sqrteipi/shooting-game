1b04c9179073a2cb3c92986e46e1197ad4a47f69
sqrteipi <wongcheukiu@gmail.com>
2024-09-06 01:12:49 +0800

    Bullets added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Example file showing a circle moving on screen
 import pygame
+from math import radians, sin, cos
 
 # pygame setup
 pygame.init()
@@ -9,6 +10,12 @@ running = True
 dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+rect_pos = pygame.Vector2(1280 * 3 / 4, 720 / 2)
+lines = []
+line_len = 75
+line_spd = 10
+time = 0
+
 while running:
 
     for event in pygame.event.get():
@@ -18,7 +25,7 @@ while running:
     screen.fill("black")
 
     # player properties
-    pygame.draw.circle(screen, "white", player_pos, 40)
+    pygame.draw.circle(screen, "white", player_pos, 30)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -31,7 +38,23 @@ while running:
         player_pos.x += 300 * dt
 
     # enemy properties
-    pygame.draw.rect(screen, "white", (620, 340, 40, 40))
+    pygame.draw.rect(screen, "white", (rect_pos.x - 20, rect_pos.y - 20, 40, 40))
+    
+    if time >= 100 and time % 50 == 0:
+        for deg in range(0, 360, 45):
+            lines.append([rect_pos.x, rect_pos.y, deg])
+
+    nxt_lines = []
+    for line in lines:
+        line_pos, line_bearing = pygame.Vector2(line[0], line[1]), line[2]
+        end_pos = (line_pos.x + line_len * cos(radians(line_bearing)), line_pos.y - line_len * sin(radians(line_bearing)))
+        pygame.draw.line(screen, "white", line_pos, end_pos, 5)
+        line_pos.x += line_spd * cos(radians(line_bearing))
+        line_pos.y -= line_spd * sin(radians(line_bearing))
+        if line_pos.x >= 0 and line_pos.x <= 1280 and line_pos.y >= 0 and line_pos.y <= 720:
+            nxt_lines.append([line_pos.x, line_pos.y, line_bearing])
+    
+    lines = nxt_lines
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -40,5 +63,6 @@ while running:
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
     dt = clock.tick(60) / 1000
+    time += 1
 
 pygame.quit()
